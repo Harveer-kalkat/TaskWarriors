1cfa829c9bdcb76855788609bdac63ab08cb0af3
Harveer-kalkat <harrykalkat@gmail.com>
2023-06-16 16:24:08 -0700

    Popular service and popular warriors sections created

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,35 +1,43 @@
-import React from 'react'
-import { ButtonGroup, Container, IconButton, Stack, Text } from '@chakra-ui/react'
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import React from "react";
+import { ButtonGroup, IconButton, Stack, Text, Box } from "@chakra-ui/react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Container bg='#7B70F6' as="footer" role="contentinfo" py={{ base: '12', md: '16' }}>
-    <Stack spacing={{ base: '4', md: '5' }}>
-      <Stack justify="space-between" direction="row" align="center">
-        <Text as='p'>TaskWarriors</Text>
-        <ButtonGroup variant="tertiary">
-          <IconButton
-            as="a"
-            href="#"
-            aria-label="LinkedIn"
-            icon={<FaLinkedin fontSize="1.25rem" />}
-          />
-          <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
-          <IconButton
-            as="a"
-            href="#"
-            aria-label="Twitter"
-            icon={<FaTwitter fontSize="1.25rem" />}
-          />
-        </ButtonGroup>
-      </Stack>
-      <Text fontSize="sm" color="fg.subtle">
-        &copy; {new Date().getFullYear()} Chakra UI Pro, Inc. All rights reserved.
-      </Text>
-    </Stack>
-  </Container>
-  )
-}
+    <Box bg="#7B70F6" as="footer" role="contentinfo" px="90px">
+      <Stack spacing={{ base: "4", md: "5" }}>
+        <Stack justify="space-between" direction="row" align="center">
+          <Text as="p">TaskWarriors</Text>
 
-export default Footer
+          <ButtonGroup variant="tertiary">
+            <IconButton
+              as="a"
+              href="#"
+              aria-label="LinkedIn"
+              icon={<FaLinkedin fontSize="1.25rem" />}
+            />
+
+            <IconButton
+              as="a"
+              href="#"
+              aria-label="GitHub"
+              icon={<FaGithub fontSize="1.25rem" />}
+            />
+            <IconButton
+              as="a"
+              href="#"
+              aria-label="Twitter"
+              icon={<FaTwitter fontSize="1.25rem" />}
+            />
+          </ButtonGroup>
+        </Stack>
+        <Text fontSize="sm" color="fg.subtle" pb="5px">
+          &copy; {new Date().getFullYear()} TaskWarriros, Inc. All rights
+          reserved.
+        </Text>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
